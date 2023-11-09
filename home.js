9b217b62d3fbe1fd7c4e101c6a6a9780ad59c729
Kishaun13 <Kishaunbrown14@gmail.com>
2023-11-09 10:43:03 -0500

    Added checkwin fuction checkdraw function and endgame draw along with restart game function

--- home.js
+++ home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const board = document.querySelector('.Game');
 const cells = document.querySelectorAll('.tile');
 const player1wins = document.querySelector('.player1wins');
@@ -47,84 +46,39 @@ function handleCellClick(event) {
     }
 }
 
-function clearBoard() {
-    cells.forEach(cell => {
-        cell.textContent = '';
-=======
-window.addEventListener('DOMContentLoaded', () => {
-    var tiles = Array.from(document.querySelectorAll('.tile'));
-    var playerDisplay = document.querySelector('.display-player');
-
-    let grid = ['', '', '', '', '', '', '', '', ''];
-    let currentPlayer = 'X';
-    let isGameActive = true;
-
-
-    var winPositions = [
-        [0, 1, 2,],
-        [3, 4, 5,],
-        [6, 7, 8,],
-        [0, 3, 6,],
-        [1, 4, 7,],
-        [2, 5, 8,],
-        [0, 4, 8,],
-        [2, 4, 6,]
-    ];
-
-    var isValidAction = (tile) => {
-        if (tile.innerText === 'X' || tile.innerText === 'O'){
-            return false;
+function checkWin() {
+    for (let i = 0; i < winningCombinations.length; i++) {
+        const [a, b, c] = winningCombinations[i];
+        if (gameState[a] !== '' && gameState[a] === gameState[b] && gameState[b] === gameState[c]) {
+            return true;
         }
-    
-        return true;
-    };
-
-    var updateBoard =  (index) => {
-        board[index] = currentPlayer;
-     }
-
-    var changePlayer = () => {
-        playerDisplay.classList.remove(`player${currentPlayer}`);
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        playerDisplay.innerText = currentPlayer;
-        playerDisplay.classList.add(`player${currentPlayer}`);
     }
-
-    function isValidResult() {
-        let win = false;
-        for (let i = 0; i <= 7; i++) {
-            var winCondition = winPositions[i];
-            var a = grid[winCondition[0]];
-            var b = grid[winCondition[1]];
-            var c = grid[winCondition[2]];
-            if (a === "" || b === "" || c ==="") {
-                continue;
-            }
-            if (a === b && b === c) {
-                win = true;
-                break;
-            }
-        }
-
-    }
-
-    var userAction = (tile, index) => {
-        if (isValidAction(tile) && isGameActive) {
-            tile.innerText = currentPlayer;
-            tile.classList.add(`player${currentPlayer}`);
-            updateBoard(index);
-            isValidResult();
-            changePlayer();
-        }
-    };
-    
-    tiles.forEach( (tile, index) => {
-        tile.addEventListener('click', () => userAction(tile, index));
->>>>>>> 5d10d6f9aa74d74ace9322a13bbcdda010ab3e73
-    });
+    return false;
 }
-    
 
+
+function checkDraw() {
+    return gameState.every(cell => cell !== '');
+}
+
+
+
+function endGame(draw) {
+    if (draw) {
+        document.querySelector('.Game').classList.add('draw');
+    } else {
+        document.querySelector('.Game').classList.add('winner');
+    }
+    gameActive = false;
+}
+function restartGame() {
+    gameActive = true;
+    currentPlayer = 'X';
+    gameState = ['', '', '', '', '', '', '', '', ''];
+    document.querySelectorAll('.tile').forEach(cell => cell.textContent = '');
+    document.querySelector('.Game').classList.remove('winner');
+    document.querySelector('.Game').classList.remove('draw');
+}
 // window.addEventListener('DOMContentLoaded', () => {
 //     var tiles = Array.from(document.querySelectorAll('.tile'));
 //     var playerDisplay = document.querySelector('.display-player');
@@ -195,55 +149,125 @@ window.addEventListener('DOMContentLoaded', () => {
 //     tiles.forEach( (tile, index) => {
 //         tile.addEventListener('click', () => userAction(tile, index));
 //     });
+// }
+    
+
+// // window.addEventListener('DOMContentLoaded', () => {
+// //     var tiles = Array.from(document.querySelectorAll('.tile'));
+// //     var playerDisplay = document.querySelector('.display-player');
+
+// //     let grid = ['', '', '', '', '', '', '', '', ''];
+// //     let currentPlayer = 'X';
+// //     let isGameActive = true;
+
+
+// //     var winPositions = [
+// //         [0, 1, 2,],
+// //         [3, 4, 5,],
+// //         [6, 7, 8,],
+// //         [0, 3, 6,],
+// //         [1, 4, 7,],
+// //         [2, 5, 8,],
+// //         [0, 4, 8,],
+// //         [2, 4, 6,]
+// //     ];
+
+// //     var isValidAction = (tile) => {
+// //         if (tile.innerText === 'X' || tile.innerText === 'O'){
+// //             return false;
+// //         }
+    
+// //         return true;
+// //     };
+
+// //     var updateBoard =  (index) => {
+// //         board[index] = currentPlayer;
+// //      }
+
+// //     var changePlayer = () => {
+// //         playerDisplay.classList.remove(`player${currentPlayer}`);
+// //         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+// //         playerDisplay.innerText = currentPlayer;
+// //         playerDisplay.classList.add(`player${currentPlayer}`);
+// //     }
+
+// //     function isValidResult() {
+// //         let win = false;
+// //         for (let i = 0; i <= 7; i++) {
+// //             var winCondition = winPositions[i];
+// //             var a = grid[winCondition[0]];
+// //             var b = grid[winCondition[1]];
+// //             var c = grid[winCondition[2]];
+// //             if (a === "" || b === "" || c ==="") {
+// //                 continue;
+// //             }
+// //             if (a === b && b === c) {
+// //                 win = true;
+// //                 break;
+// //             }
+// //         }
+
+// //     }
+
+// //     var userAction = (tile, index) => {
+// //         if (isValidAction(tile) && isGameActive) {
+// //             tile.innerText = currentPlayer;
+// //             tile.classList.add(`player${currentPlayer}`);
+// //             updateBoard(index);
+// //             isValidResult();
+// //             changePlayer();
+// //         }
+// //     };
+    
+// //     tiles.forEach( (tile, index) => {
+// //         tile.addEventListener('click', () => userAction(tile, index));
+// //     });
 
       
-<<<<<<< HEAD
+// //   });
 //   });
-=======
-  });
-const board = document.querySelector('.Game');
-const cells = document.querySelectorAll('.tile');
+// const board = document.querySelector('.Game');
+// const cells = document.querySelectorAll('.tile');
 
-let currentPlayer = 'X';
-let gameActive = true;
-let gameState = ['', '', '', '', '', '', '', '', ''];
+// let currentPlayer = 'X';
+// let gameActive = true;
+// let gameState = ['', '', '', '', '', '', '', '', ''];
 
-const winningCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+// const winningCombinations = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6]
+// ];
 
-cells.forEach((cell, index) => {
-    cell.setAttribute('data-index', index);
-    cell.addEventListener('click', handleCellClick);
-});
+// cells.forEach((cell, index) => {
+//     cell.setAttribute('data-index', index);
+//     cell.addEventListener('click', handleCellClick);
+// });
 
-function handleCellClick(event) {
-    const cell = event.target;
-    const index = parseInt(cell.getAttribute('data-index'));
+// function handleCellClick(event) {
+//     const cell = event.target;
+//     const index = parseInt(cell.getAttribute('data-index'));
 
-    if (gameState[index] !== '' || !gameActive) {
-        return;
-    }
+//     if (gameState[index] !== '' || !gameActive) {
+//         return;
+//     }
 
-    gameState[index] = currentPlayer;
-    cell.textContent = currentPlayer;
+//     gameState[index] = currentPlayer;
+//     cell.textContent = currentPlayer;
 
-    const win = checkWin();
-    const draw = checkDraw();
+//     const win = checkWin();
+//     const draw = checkDraw();
 
-    if (win) {
-        endGame(false);
-    } else if (draw) {
-        endGame(true);
-    } else {
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    }
-}
->>>>>>> 5d10d6f9aa74d74ace9322a13bbcdda010ab3e73
+//     if (win) {
+//         endGame(false);
+//     } else if (draw) {
+//         endGame(true);
+//     } else {
+//         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+//     }
+// }
