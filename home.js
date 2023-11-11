abdd25cfd5c2d5d09c3be8507932e34256676418
Kishaun13 <Kishaunbrown14@gmail.com>
2023-11-10 22:04:01 -0500

    fixed keepscore and keep ties functions

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 const board = document.querySelector(".Game");
 const cells = document.querySelectorAll(".tile");
-const player1wins = document.querySelector("#player1wins");
-const player2wins = document.querySelector("#player2wins");
+const player1win = document.querySelector("#player1wins");
+const player2win = document.querySelector("#player2wins");
 const tiesDisplay = document.querySelector("#ties");
 
 let currentPlayer = "X";
@@ -73,23 +73,21 @@ function resetGame() {
 console.log(player1wins, player2wins, tiesDisplay);
 
 function keepScore() {
-    if (currentPlayer === "X") {
-      player1wins++;
-      player1wins.textContent = player1wins;
-    } else if (currentPlayer === "O") {
-      player2wins++;
-      player2wins.textContent = player2wins;
-    } else {
-      ties++;
-      tiesDisplay.textContent = ties;
-    }
+  if (currentPlayer === "X") {
+    let score = parseInt(player1win.textContent);
+    player1win.textContent = score + 1;
+  } else if (currentPlayer === "O") {
+    let score = parseInt(player2win.textContent);
+    player2win.textContent = score + 1;
   }
+}
 
 
   function keepTies() {
-    ties++;
-    tiesDisplay.textContent = ties;
+    let score = parseInt(tiesDisplay.textContent);
+    tiesDisplay.textContent = score + 1
   }
+  
   function endGame(draw) {
     gameActive = false;
     if (draw) {
@@ -99,147 +97,3 @@ function keepScore() {
     }
   }
   
-// function clearBoard() {
-//     cells.forEach(cell => {
-//         cell.textContent = '';
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     var tiles = Array.from(document.querySelectorAll('.tile'));
-//     var playerDisplay = document.querySelector('.display-player');
-
-//     let grid = ['', '', '', '', '', '', '', '', ''];
-//     let currentPlayer = 'X';
-//     let isGameActive = true;
-
-//     var winPositions = [
-//         [0, 1, 2,],
-//         [3, 4, 5,],
-//         [6, 7, 8,],
-//         [0, 3, 6,],
-//         [1, 4, 7,],
-//         [2, 5, 8,],
-//         [0, 4, 8,],
-//         [2, 4, 6,]
-//     ];
-
-//     var isValidAction = (tile) => {
-//         if (tile.innerText === 'X' || tile.innerText === 'O'){
-//             return false;
-//         }
-
-//         return true;
-//     };
-
-//     var updateBoard =  (index) => {
-//         board[index] = currentPlayer;
-//      }
-
-//     var changePlayer = () => {
-//         playerDisplay.classList.remove(`player${currentPlayer}`);
-//         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-//         playerDisplay.innerText = currentPlayer;
-//         playerDisplay.classList.add(`player${currentPlayer}`);
-//     }
-
-//     function isValidResult() {
-//         let win = false;
-//         for (let i = 0; i <= 7; i++) {
-//             var winCondition = winPositions[i];
-//             var a = grid[winCondition[0]];
-//             var b = grid[winCondition[1]];
-//             var c = grid[winCondition[2]];
-//             if (a === "" || b === "" || c ==="") {
-//                 continue;
-//             }
-//             if (a === b && b === c) {
-//                 win = true;
-//                 break;
-//             }
-//         }
-
-//     }
-
-//     var userAction = (tile, index) => {
-//         if (isValidAction(tile) && isGameActive) {
-//             tile.innerText = currentPlayer;
-//             tile.classList.add(`player${currentPlayer}`);
-//             updateBoard(index);
-//             isValidResult();
-//             changePlayer();
-//         }
-//     };
-
-//     tiles.forEach( (tile, index) => {
-//         tile.addEventListener('click', () => userAction(tile, index));
-
-//     });
-// }
-// window.addEventListener('DOMContentLoaded', () => {
-//     var tiles = Array.from(document.querySelectorAll('.tile'));
-//     var playerDisplay = document.querySelector('.display-player');
-
-//     let grid = ['', '', '', '', '', '', '', '', ''];
-//     let currentPlayer = 'X';
-//     let isGameActive = true;
-
-//     var winPositions = [
-//         [0, 1, 2,],
-//         [3, 4, 5,],
-//         [6, 7, 8,],
-//         [0, 3, 6,],
-//         [1, 4, 7,],
-//         [2, 5, 8,],
-//         [0, 4, 8,],
-//         [2, 4, 6,]
-//     ];
-
-//     var isValidAction = (tile) => {
-//         if (tile.innerText === 'X' || tile.innerText === 'O'){
-//             return false;
-//         }
-
-//         return true;
-//     };
-
-//     var updateBoard =  (index) => {
-//         board[index] = currentPlayer;
-//      }
-
-//     var changePlayer = () => {
-//         playerDisplay.classList.remove(`player${currentPlayer}`);
-//         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-//         playerDisplay.innerText = currentPlayer;
-//         playerDisplay.classList.add(`player${currentPlayer}`);
-//     }
-
-//     function isValidResult() {
-//         let win = false;
-//         for (let i = 0; i <= 7; i++) {
-//             var winCondition = winPositions[i];
-//             var a = grid[winCondition[0]];
-//             var b = grid[winCondition[1]];
-//             var c = grid[winCondition[2]];
-//             if (a === "" || b === "" || c ==="") {
-//                 continue;
-//             }
-//             if (a === b && b === c) {
-//                 win = true;
-//                 break;
-//             }
-//         }
-
-//     }
-
-//     var userAction = (tile, index) => {
-//         if (isValidAction(tile) && isGameActive) {
-//             tile.innerText = currentPlayer;
-//             tile.classList.add(`player${currentPlayer}`);
-//             updateBoard(index);
-//             isValidResult();
-//             changePlayer();
-//         }
-//     };
-
-//     tiles.forEach( (tile, index) => {
-//         tile.addEventListener('click', () => userAction(tile, index));
-//     });
